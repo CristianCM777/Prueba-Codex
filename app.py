ce3efed2a51cceab8244b47b4366019b196b0639
Camarillo Molina Cristian <l23210553@tectijuana.edu.mx>
2026-04-28 17:26:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+print("Calculadora básica")
 
+a = int(input("Número 1: "))
+b = int(input("Número 2: "))
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+
+if b != 0:
+    print("División:", a / b)
+else:
+    print("No se puede dividir entre cero")
